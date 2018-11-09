03be3cf926eb19a06c20cd71a5537e389d20d61b
Max Chen <immaxchen@gmail.com>
2018-11-10 01:11:34 +0800

    provide a compatible class definition example

---
+++ User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Example
+{
+    class User
+    {
+        public int? UserId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime? Birthday { get; set; }
+
+        public User(int? UserId, string Name, string Gender, DateTime? Birthday)
+        {
+            this.UserId = UserId;
+            this.Name = Name;
+            this.Gender = Gender;
+            this.Birthday = Birthday;
+        }
+
+        public User()
+        {
+        }
+    }
+}
